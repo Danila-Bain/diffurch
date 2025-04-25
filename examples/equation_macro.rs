14513e741dcc12ff127a487ddaf6aa201dda8efa
Danila Bain <danila.bain@yandex.com>
2025-04-25 12:05:13 +0300

    Readme cleanup

---
+++ examples/equation_macro.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // equation!{
+    //     Lorenz {
+    //         x' = sigma * (y - x), 
+    //         y' = x * (rho - z) - y, 
+    //         z' = x * y - beta * z,
+    //     }
+    // }
+
+    // equation!{
+    //     BouncingBall {
+    //         x'' = -g,
+    //         x < 0 => x' = -k * x',
+    //     }
+    // }
+    todo!()
+}
