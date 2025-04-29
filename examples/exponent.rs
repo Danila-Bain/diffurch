0e720093355543b9f87c6089536faec6108ebb20
Danila Bain <danila.bain@yandex.com>
2025-04-29 17:54:56 +0300

    exponent (x' = k x) example

---
+++ examples/exponent.rs
@@ -0,0 +1,14 @@
+use diffurch::*;
+
+fn main() {
+    let k = f64::ln(2.);
+    let eq = Equation::ode(move |[x]| [k * x]).with_delay(100.);
+    let ic = |t: f64| [(k * t).exp()];
+
+    Solver::new()
+        .rk(&rk::RK98)
+        .stepsize(0.1)
+        .on_step(Event::ode2(|t, [x]| (t, x, (x - ic(t)[0]) / x)).to_std())
+        // .on_step(Event::dde(|t, [x], [x_]| (t, x, (x - ic(t)[0]) / x, x_(t - 1.))).to_std())
+        .run(eq, ic, 0. ..5.);
+}
