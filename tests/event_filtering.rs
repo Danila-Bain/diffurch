938eaeaf4ea1449a20204852725251320c67bf09
Danila Bain <danila.bain@yandex.com>
2025-05-11 12:27:14 +0300

    clean up useless tests

--- tests/event_filtering.rs
+++ tests/event_filtering.rs
@@ -1,5 +1,4 @@
-use diffurch::{polynomial, InitialCondition};
-use diffurch::{Equation, Event, Solver, rk};
+use diffurch::*;
 
 #[test]
 fn main() {
@@ -11,45 +10,65 @@ fn main() {
     let f = |t: f64, [x]: [f64; 1]| (t, x);
 
     let mut p = Vec::new();
-    let mut p_e2 = Vec::new();
-    let mut p_e2_e2 = Vec::new();
-    let mut p_s4 = Vec::new();
-    let mut p_r = Vec::new();
-    let mut p_r_e3 = Vec::new();
-    let mut p_e3_r = Vec::new();
-    let mut p_t_5_10 = Vec::new();
-    let mut p_t_0_oo = Vec::new();
-    let mut p_t_0_5 = Vec::new();
-    let mut p_take_5 = Vec::new();
-    let mut p_g_100 = Vec::new();
-    let mut p_g_100_last = (0., 0.);
-    let mut p_g_100_first = (0., 0.);
-    let mut p13 = Vec::new();
-    let mut p14 = Vec::new();
-
     let event = Event::ode2(f).to_vec(&mut p);
+
+    let mut p_e2 = Vec::new();
     let event_e2 = Event::ode2(f).to_vec(&mut p_e2).every(2);
+
+    let mut p_e2_e2 = Vec::new();
     let event_e2_e2 = Event::ode2(f).to_vec(&mut p_e2_e2).every(2).every(2);
+
+    let mut p_s4 = Vec::new();
     let event_s4 = Event::ode2(f).to_vec(&mut p_s4).separated_by(4.);
+
+    let mut p_r = Vec::new();
     let event_r = Event::ode2(f).to_vec(&mut p_r).in_range(10. ..20.);
-    let event_r_e3 = Event::ode2(f).to_vec(&mut p_r_e3).in_range(10. ..20.).every(3);
-    let event_e3_r = Event::ode2(f).to_vec(&mut p_e3_r).every(3).in_range(10. ..20.);
+
+    let mut p_r_e3 = Vec::new();
+    let event_r_e3 = Event::ode2(f)
+        .to_vec(&mut p_r_e3)
+        .in_range(10. ..20.)
+        .every(3);
+
+    let mut p_e3_r = Vec::new();
+    let event_e3_r = Event::ode2(f)
+        .to_vec(&mut p_e3_r)
+        .every(3)
+        .in_range(10. ..20.);
+
+    let mut p_t_5_10 = Vec::new();
     let event_t_5_10 = Event::ode2(f).to_vec(&mut p_t_5_10).times(5..10);
+
+    let mut p_t_0_oo = Vec::new();
     let event_t_0_oo = Event::ode2(f).to_vec(&mut p_t_0_oo).times(0..usize::MAX);
+
+    let mut p_t_0_5 = Vec::new();
     let event_t_0_5 = Event::ode2(f).to_vec(&mut p_t_0_5).times(0..5);
+
+    let mut p_take_5 = Vec::new();
     let event_take_5 = Event::ode2(f).to_vec(&mut p_take_5).take(5);
+
+    let mut p_g_100 = Vec::new();
     let event_g_100 = Event::ode2(f)
         .to_vec(&mut p_g_100)
         .filter_by_ode(|[x]| x > 100.);
+
+    let mut p_g_100_last = (0., 0.);
     let event_g_100_last = Event::ode2(f)
         .to_var(&mut p_g_100_last)
         .filter_by_ode(|[x]| x > 100.);
+
+    let mut p_g_100_first = (0., 0.);
     let event_g_100_first = Event::ode2(f)
         .to_var(&mut p_g_100_first)
         .filter_by_ode(|[x]| x > 100.)
         .once();
-    let event13 = Event::ode2(f).to_vec(&mut p13).subdivide(2);
-    let event14 = Event::ode2(f).to_vec(&mut p14).subdivide(4);
+
+    let mut p_sub2 = Vec::new();
+    let event_sub2 = Event::ode2(f).to_vec(&mut p_sub2).subdivide(2);
+
+    let mut p_sub4 = Vec::new();
+    let event_sub4 = Event::ode2(f).to_vec(&mut p_sub4).subdivide(4);
 
     Solver::rk(&rk::RK98) // it is exact for polynomials up to 8th or 9th order
         .stepsize(1.)
@@ -67,8 +86,8 @@ fn main() {
         .on_step(event_g_100)
         .on_step(event_g_100_last)
         .on_step(event_g_100_first)
-        .on_step(event13)
-        .on_step(event14)
+        .on_step(event_sub2)
+        .on_step(event_sub4)
         .run(eq, ic, interval);
 
     let f_i = |i| (i as f64, solution(i as f64)[0]);
@@ -96,7 +115,7 @@ fn main() {
         (0..=100)
             .map(|i| {
                 let t = i as f64 * 0.5;
-                (p13[i + 1].1 - f_t(t).1).abs()
+                (p_sub2[i + 1].1 - f_t(t).1).abs()
             })
             .fold(0f64, |acc, x| acc.max(x))
             < 1e-7
@@ -105,11 +124,9 @@ fn main() {
         (0..=200)
             .map(|i| {
                 let t = i as f64 * 0.25;
-                (p14[i + 3].1 - f_t(t).1).abs()
+                (p_sub4[i + 3].1 - f_t(t).1).abs()
             })
             .fold(0f64, |acc, x| acc.max(x))
             < 1e-7
     );
 }
-
-
