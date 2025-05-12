eb11155ef3b6a75c4d4d5490a6f4e1ddc4dd6df1
Danila Bain <danila.bain@yandex.com>
2025-05-12 20:11:36 +0300

    doc(initial_conditions)

--- src/initial_condition.rs
+++ src/initial_condition.rs
@@ -1,6 +1,14 @@
+//! Defines [InitialCondition].
+
+/// Container of initial conditions.
 pub enum InitialCondition<'a, const N: usize> {
+    /// Represents an initial value (for ordinary differential equations) or a constant function (for (neutral) delay differential equations)
     Point([f64; N]),
+    /// Represents an initial function (for delay differential equations)
     Function(Box<dyn 'a + Fn(f64) -> [f64; N]>),
+    /// Represents an initial function (for delay differential equations), together with its
+    /// derivative. The derivative of the initial function is required for neutral delay
+    /// differential equations.
     FunctionWithDerivative(
         Box<dyn 'a + Fn(f64) -> [f64; N]>,
         Box<dyn 'a + Fn(f64) -> [f64; N]>,
@@ -8,6 +16,10 @@ pub enum InitialCondition<'a, const N: usize> {
 }
 
 impl<'a, const N: usize> InitialCondition<'a, N> {
+    /// Evaluate the initial conditions at a given time.
+    ///
+    /// For Point variant, it is just its value,
+    /// Function or FunctionWithDerivative, it is the value of a function at that time.
     pub fn eval(&self, t: f64) -> [f64; N] {
         match self {
             &Self::Point(value) => value,
@@ -15,6 +27,11 @@ impl<'a, const N: usize> InitialCondition<'a, N> {
         }
     }
 
+    /// Evaluate the derivative of the inital condition at a given time.
+    ///
+    /// For Point variant, it is zero array.
+    /// For Function variant, it panics.
+    /// For FunctionWithDerivative variant, the second function is evaluated.
     pub fn eval_d(&self, t: f64) -> [f64; N] {
         match &self {
             &Self::Point(_) => [0.; N],
