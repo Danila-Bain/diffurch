a00f1d6667caf3955baa1011b741e9f8f457bad0
Danila Bain <danila.bain@yandex.com>
2025-05-12 20:01:24 +0300

    doc(equation)

--- src/equation.rs
+++ src/equation.rs
@@ -1,13 +1,56 @@
+//! This module defines the struct [Equation], which holds the right hand side of the equation.
+//!
+
 use crate::{StateCoordFnTrait, StateFn};
 
-// use crate::state::CoordFn;
-//
+/// The struct that is used to hold the right hand side of the function.
+///
+/// [Equation] provides convenience constructor which accept closure
+/// representations of the equation, with the types of arguments deduced.
+///
+/// # Examples
+/// ```rust
+/// // right hand side (RHS) is a constant:
+/// let constant = Equation::constant(|| [1.]); 
+/// // RHS is a known time function:
+/// let time = Equation::time(|t| [t.sin(), t.cos()]) 
+/// // RHS of an autonomous ODE:
+/// let ode = Equation::ode(|[x, y]| [-y, x]) 
+/// // RHS of a non-autonomous ODE:
+/// let ode2 = Equation::ode2(|t, [x, y]| [-y / t, x * t]) 
+/// // RHS of a delay differential equation:
+/// let dde = Equation::dde(|t, [x], [x_]| [4. * x * (1. - x_(t - 1.))]) 
+/// // RHS of a neutral delay differential equation:
+/// let ndde = Equation::dde(|t, [x], [x_]| [4. * x * (1. - x_.d(t - 1.))]) 
+/// ```
+///
 pub struct Equation<'a, const N: usize = 1> {
+    /// The right-hand-side of the function, a function that acts on
+    /// [crate::State].
     pub rhs: StateFn<'a, N, [f64; N]>,
+    /// The maximal delay, that is present in the equation.
+    ///
+    /// By default, it is zero for ordinary differential equations, and  `f64::INFINITY` for delay
+    /// differential equations.
+    /// 
+    /// In delay differential equations, the past state of the solution must be stored. But
+    /// usually, only recent history is needed, and this field tells the solver, that the history
+    /// this old won't be needed. Solver panics, if needed solution history turns out to be deleted. 
+    ///
+    /// If you need to integrate delay differential equations for long, you might want to set this field using
+    /// [Equation::with_delay] method, to avoid excessive memory usage.
+    ///
+    /// Also, if you use events that compute something non-local, i.e. the amplitude of the
+    /// periodic solution, or variance on the part of the solution, you might need to access past
+    /// states, in which case you also might need to set this field to a larger value with
+    /// [Equation::with_delay]
+    ///
     pub max_delay: f64,
 }
 
 impl<'a, const N: usize> Equation<'a, N> {
+
+    /// Constructor that accepts [StateFn] and sets [Equation::max_delay] to `f64::NAN`.
     pub fn new(rhs: StateFn<'a, N, [f64; N]>) -> Self {
         Equation {
             rhs,
@@ -15,6 +58,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Constructor that accepts a `Fn() -> [f64; N]` closure and sets [Equation::max_delay] to `0`.
     pub fn constant<RHS>(rhs: RHS) -> Self
     where
         RHS: 'a + Fn<(), Output = [f64; N]>,
@@ -25,6 +69,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Constructor that accepts a `Fn(f64) -> [f64; N]` closure and sets [Equation::max_delay] to `0`. 
     pub fn time<RHS>(rhs: RHS) -> Self
     where
         RHS: 'a + Fn<(f64,), Output = [f64; N]>,
@@ -35,6 +80,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Constructor that accepts a `Fn([f64; N]) -> [f64; N]` closure and sets [Equation::max_delay] to `0`.
     pub fn ode<RHS>(rhs: RHS) -> Self
     where
         RHS: 'a + Fn<([f64; N],), Output = [f64; N]>,
@@ -45,6 +91,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Constructor that accepts a `Fn(f64, [f64; N]) -> [f64; N]` closure and sets [Equation::max_delay] to `0`.
     pub fn ode2<RHS>(rhs: RHS) -> Self
     where
         RHS: 'a + Fn<(f64, [f64; N]), Output = [f64; N]>,
@@ -55,6 +102,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Constructor that accepts a `Fn(f64, [f64; N], ) -> [f64; N]` closure and sets [Equation::max_delay] to `f64::INFINITY`.
     pub fn dde<RHS>(rhs: RHS) -> Self
     where
         RHS: 'a + Fn(f64, [f64; N], [Box<dyn '_ + StateCoordFnTrait>; N]) -> [f64; N],
@@ -65,6 +113,7 @@ impl<'a, const N: usize> Equation<'a, N> {
         }
     }
 
+    /// Sets [Equation::max_delay] and returns Self
     pub fn with_delay(self, value: f64) -> Self {
         Self {
             rhs: self.rhs,
