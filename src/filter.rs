ed567c6777a260aee322bcc1c0aa292c4a59c505
Danila Bain <danila.bain@yandex.com>
2025-05-17 14:46:06 +0300

    doc(filter) methods

--- src/filter.rs
+++ src/filter.rs
@@ -28,6 +28,9 @@ where
         self.filter(StateFn::ode2(f))
     }
 
+    /// Push a new filtering function, which returns `true` every `n`th invocation, including the
+    /// first invocation. 
+    /// The effect of that filtration, is that only every 'n'th event is remained.
     fn every(self, n: usize) -> Self {
         let mut counter = n - 1;
         self.filter_constant(move || {
@@ -37,6 +40,10 @@ where
         })
     }
 
+    /// Push a new filtering function, which returns `true` every `n`th invocation, starting with
+    /// `(offset % n)`th invocation, such that with `offset = 0` it is equivalent to [Filter::every].
+    /// The effect of that filtration, is that only every 'n'th event is remained, starting from
+    /// `offset`th event (zero-based).
     fn every_offset(self, n: usize, offset: usize) -> Self {
         let offset = offset % n;
         let mut counter = n - 1 - offset;
@@ -47,6 +54,10 @@ where
         })
     }
 
+    /// Push a new filtering function, which remembers the last time it returned `true` and returns
+    /// `true` only if the state time is advanced at least by `delta` (inclusive) since the last
+    /// returning `true`. The effect is that after filtration events are guaranteed to be separated
+    /// in time by at least delta.
     fn separated_by(self, delta: f64) -> Self {
         let mut last_trigger = f64::NEG_INFINITY;
         self.filter_time(move |t| {
@@ -59,10 +70,14 @@ where
         })
     }
 
+    /// Push a new filtering function, that filters all events, the state time of which is not
+    /// contained in a given f64 range.
     fn in_range(self, interval: impl 'a + std::ops::RangeBounds<f64>) -> Self {
         self.filter_time(move |t| interval.contains(&t))
     }
 
+    /// Push a filtering function, that returns `true` on the first invocation and then always
+    /// returns `false`. Equivalent in behaviour to [Filter::take(1)].
     fn once(self) -> Self {
         let mut flag = true;
         self.filter_constant(move || {
@@ -75,6 +90,8 @@ where
         })
     }
 
+    /// Push a filtering function, that returns `true` on the first `n` invocations and then always
+    /// returns `false`. Equivalent in behaviour to [Filter::times(0..n)].
     fn take(self, n: usize) -> Self {
         let mut counter = 0;
         self.filter_constant(move || {
@@ -83,6 +100,8 @@ where
         })
     }
 
+    /// Push a filtering function, that returns `true` if the number of the invocation of that
+    /// function is in `range`.
     fn times(self, range: impl 'a + std::ops::RangeBounds<usize>) -> Self {
         let mut counter = 0;
         self.filter_constant(move || {
