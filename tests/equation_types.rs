01d014617e618dc5b12b1f652db38bed2edbd952
Danila Bain <danila.bain@yandex.com>
2025-11-15 12:22:27 +0300

    some equation tests

--- tests/equation_types.rs
+++ tests/equation_types.rs
@@ -4,81 +4,98 @@
 use core::f64;
 
 use diffurch::*;
+use StateRef as S;
 
 #[test]
 fn constant() {
     let solution = |t: f64| [t, -t, 0.];
 
     Solver::new()
-        .equation(StateFn::new(|&StateRef {..}| [1., -1., 0.]))
+        .equation(StateFn::new(|&S { .. }| [1., -1., 0.]))
         .initial([0., 0., 0.])
         .interval(0. ..10.)
         .stepsize(0.25)
         .rk(rk::euler())
-        .on_step(StateFn::new(|&StateRef{ t: &t, x: &x, .. }| assert_eq!(x, solution(t))))
+        .on_step(StateFn::new(|&S { t, x, .. }| {
+            assert_eq!(x, solution(t))
+        }))
         .run();
 }
 
-// #[test]
-// fn time() {
-//     let solution = |t: f64| [0., t, t * t / 2.];
-//
-//     Solver::new()
-//         .equation(state_fn!(|t| [0., 1., t]))
-//         .initial([0., 0., 0.])
-//         .interval(0. ..10.)
-//         .rk(&rk::RK98)
-//         .stepsize(0.5)
-//         .on_step(event!(|t, [x, y, z]| assert_eq!([x, y, z], solution(t))))
-//         .run();
-// }
-//
-// #[test]
-// fn ode_exponent() {
-//     let solution = |t: f64| (-t).exp();
-//
-//     Solver::new()
-//         .equation(state_fn!(|[x]| [-x]))
-//         .initial([1.])
-//         .interval(0. ..10.)
-//         .rk(&rk::RK98)
-//         .stepsize(0.1)
-//         .on_step(event!(|t, [x]| {
-//             assert!((x - solution(t)).abs() < 1e-14)
-//         }))
-//         .run();
-// }
-//
-// #[test]
-// fn ode_harmonic() {
-//     let solution = |t: f64| t.sin();
-//
-//     Solver::new()
-//         .equation(state_fn!(|[x, dx]| [dx, -x]))
-//         .initial([0., 1.])
-//         .interval(0. ..10.)
-//         .rk(&rk::RK98)
-//         .stepsize(0.1)
-//         .on_step(event!(|t, [x, _dx]| {
-//             assert!((x - solution(t)).abs() < 1e-13)
-//         }))
-//         .run();
-// }
-//
-// #[test]
-// fn ode2_lin() {
-//     let sol = |t: f64| t * t;
-//
-//     Solver::new()
-//         .equation(state_fn!(|t, [x]| [2. * x / t]))
-//         .initial(|t: f64| [t * t])
-//         .interval(1. ..10.)
-//         .rk(&rk::RK98)
-//         .stepsize(0.1)
-//         .on_step(event!(|t, [x]| assert!((x - sol(t)).abs() < 1e-11)))
-//         .run();
-// }
-//
+#[test]
+fn time() {
+    let solution = |t: f64| [0., t, t * t / 2.];
+
+    let mut tt = vec![];
+    let mut xx = vec![];
+
+    Solver::new()
+        .equation(StateFn::new(|&S { t, .. }| [0., 1., t]))
+        .initial([0., 0., 0.])
+        .interval(0. ..10.)
+        .rk(rk::midpoint())
+        .stepsize(0.5)
+        .on_step(StateFn::new(|&S { t, x, .. }| {
+            tt.push(t);
+            xx.push(x)
+        }))
+        .run();
+
+    for (t, x) in tt.into_iter().zip(xx.into_iter()) {
+        assert_eq!(x, solution(t));
+    }
+}
+
+#[test]
+fn ode_exponent() {
+    let solution = |t: f64| (-t).exp();
+
+    Solver::new()
+        .equation(StateFn::new(|&S { x: [x], .. }| [-1. * x]))
+        .initial([1.])
+        .interval(0. ..10.)
+        .rk(rk::rktp64())
+        .stepsize(0.05)
+        .on_step(StateFn::new(|&S { t, x: [x], .. }| {
+            assert!((x - solution(t)).abs() < 1e-14)
+        }))
+        .run();
+}
+
+#[test]
+fn ode_harmonic() {
+    let solution = |t: f64| t.sin();
+
+    Solver::new()
+        .equation(StateFn::new(|&S { x: [x, dx], .. }| [dx, -1. * x]))
+        .interval(0. ..10.)
+        .initial([0., 1.])
+        .rk(rk::rktp64())
+        .stepsize(0.04)
+        .on_step(StateFn::new(|&S { t, x: [x, _], .. }| {
+            // println!("{} =?= {}", x, solution(t));
+            assert!((x - solution(t)).abs() < 1e-13)
+        }))
+        .run();
+}
+
+#[test]
+fn odet_lin() {
+    let sol = |t: f64| t.powi(-2);
+
+    Solver::new()
+        .equation(StateFn::new(|&S { t, x: [x], .. }| [- 2. * x / t]))
+        .initial(|t: f64| [sol(t)])
+        .interval(1. ..10.)
+        .rk(rk::rktp64())
+        .stepsize(0.02)
+        .on_step(StateFn::new(|&S { t, x: [x], .. }| {
+            // println!("{}", ((x - sol(t))/sol(t)).abs());
+            assert!((x - sol(t)).abs() < 1e-13)
+        }))
+        .run();
+}
+
 // #[test]
 // fn dde_sin() {
 //     let k = 1.;
